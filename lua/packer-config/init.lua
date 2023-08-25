7476c1ca130736a5a8f570c55bf169113be6b370
Paulo Koch <pauloricardokoch@gmail.com>
2023-08-25 01:37:04 -0300

    adding basic setup and nvim-tree

---
+++ lua/packer-config/init.lua
@@ -0,0 +1,6 @@
+return require'packer'.startup(function(use)
+  use 'wbthomason/packer.nvim'
+  use 'EdenEast/nightfox.nvim'
+  use 'nvim-tree/nvim-web-devicons'
+  use 'nvim-tree/nvim-tree.lua'
+end)
---
+++ lua/settings/init.lua
@@ -0,0 +1,24 @@
+local set = vim.opt
+
+set.expandtab = true
+set.smarttab = true
+set.shiftwidth = 4
+set.tabstop = 4
+set.autoindent = true
+
+set.hlsearch = true
+set.incsearch = true
+set.ignorecase = true
+set.smartcase = true
+
+set.splitbelow = true
+set.splitright = true
+set.wrap = false
+set.scrolloff = 5
+set.termguicolors = true
+
+set.cursorline = true
+set.colorcolumn = '80'
+set.number = true 
+
+set.hidden = true
