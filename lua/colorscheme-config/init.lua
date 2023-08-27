27f53e5bbb2dea55e9ed054308c81031cfb30b99
Paulo Koch <pauloricardokoch@gmail.com>
2023-08-26 23:16:01 -0300

    changing theme

--- lua/colorscheme-config/init.lua
+++ lua/colorscheme-config/init.lua
@@ -1,3 +1,4 @@
 local cmd = vim.cmd
 
-cmd [[ colorscheme nightfox ]]
+-- cmd [[ colorscheme nightfox ]]
+cmd [[ colorscheme tokyonight ]]
--- lua/packer-config/init.lua
+++ lua/packer-config/init.lua
@@ -1,6 +1,7 @@
 return require'packer'.startup(function(use)
   use 'wbthomason/packer.nvim'
-  use 'EdenEast/nightfox.nvim'
+  -- use 'EdenEast/nightfox.nvim'
+  use 'folke/tokyonight.nvim'
   use 'nvim-tree/nvim-web-devicons'
   use 'nvim-tree/nvim-tree.lua'
   use 'nvim-lualine/lualine.nvim'
