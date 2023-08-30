7c924d964fc5db590e690ea4d0dd1ea1ceb64c97
Paulo Koch <paulo.koch@g.globo>
2023-08-30 13:38:35 -0300

    set transparent background

--- lua/colorscheme-config/init.lua
+++ lua/colorscheme-config/init.lua
@@ -1,4 +1,10 @@
 local cmd = vim.cmd
 
+require("tokyonight").setup({
+    transparent = true,
+})
+
 -- cmd [[ colorscheme nightfox ]]
 cmd [[ colorscheme tokyonight ]]
+
+cmd [[hi NvimTreeNormal guibg=NONE ctermbg=NONE]]
