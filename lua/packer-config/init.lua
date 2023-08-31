2da3a6b68e4dcc3a1272c5ec007424386257bc81
Paulo Koch <paulo.koch@g.globo>
2023-08-30 21:07:37 -0300

    adding autocompletion

--- lua/packer-config/init.lua
+++ lua/packer-config/init.lua
@@ -10,6 +10,7 @@ return require 'packer'.startup(function(use)
         'nvim-treesitter/nvim-treesitter',
         run = ':TSUpdate'
     }
+    use 'ms-jpq/coq_nvim'
     use {
         'nvim-telescope/telescope.nvim', tag = '0.1.2',
         -- or                          , branch = '0.1.x',
