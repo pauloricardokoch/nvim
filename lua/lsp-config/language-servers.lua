92a74394fcaf652b644d2c9c5521fa8808acf00b
Paulo Koch <pauloricardokoch@gmail.com>
2023-11-15 03:14:18 -0300

    added rust lsp

--- lua/lsp-config/language-servers.lua
+++ lua/lsp-config/language-servers.lua
@@ -20,6 +20,33 @@ lspconfig.gopls.setup {
         },
     },
 }
+
+local on_attach = function(client)
+    require 'completion'.on_attach(client)
+end
+
+lspconfig.rust_analyzer.setup({
+    on_attach = on_attach,
+    settings = {
+        ["rust-analyzer"] = {
+            imports = {
+                granularity = {
+                    group = "module",
+                },
+                prefix = "self",
+            },
+            cargo = {
+                buildScripts = {
+                    enable = true,
+                },
+            },
+            procMacro = {
+                enable = true
+            },
+        }
+    }
+})
+
 lspconfig.lua_ls.setup {}
 cmd('COQnow -s')
 
